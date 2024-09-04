f50830183f0ef7a6266404c53a053f100c16b097
Pierre-Noel Bouteville <pierre-noel.bouteville@allcircuits.com>
2024-09-04 11:28:36 +0200

    license

--- Makefile.mk
+++ Makefile.mk
@@ -1,1 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Pierre-Noël Bouteville <pierre-noel.bouteville@allcircuits.com>
+# SPDX-FileCopyrightText: 2024 Pierre-Noel Bouteville <pnb990@gmail.com>
+#
+# SPDX-License-Identifier: LicenseRef-ALLCircuits-ACT-1.1
+# SPDX-License-Identifier: BSD-3-Clause
+#
 LIB_STM32CUBE_FW_F4_DRIVERS_DIR:= $(dir $(lastword $(MAKEFILE_LIST)))
